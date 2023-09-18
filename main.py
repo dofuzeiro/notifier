a6aea82cdf0106b1a4755fb7968b564153553efa
Rafael Ribeiro <rafael.g.ribeiro@glintt.com>
2023-09-18 23:00:12 +0100

    Appended bike availability result to a file. Added flag that enables/disables notification thru SMS

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import datetime
 import os
 from twilio.rest import Client
 
-
 url = os.getenv('URL')
 bike_size = os.getenv('SIZE')
 to = os.getenv('TO')
 from_ = os.getenv('FROM')
 account_sid = os.getenv('ACCOUNT_SID')
 auth_token = os.getenv('AUTH_TOKEN')
+send_notification = os.getenv('SEND_NOTIFICATION')
 
 if url is None:
     raise Exception('URL must be specified')
@@ -24,7 +24,8 @@ if account_sid is None:
     raise Exception('Account_sid must be specified')
 if auth_token is None:
     raise Exception('Auth_token must be specified')
-
+if send_notification is None:
+    raise Exception('Send notification must be specified')
 
 command = [
     "curl", "-s", url,
@@ -34,18 +35,20 @@ command = [
     "tail", "-n", "1"
 ]
 
-
 try:
     result = subprocess.run(" ".join(command), check=True, capture_output=True, shell=True)
     output = result.stdout.decode('utf-8')
     client = Client(account_sid, auth_token)
 
-    if 'brevemente' not in output:
+    if 'brevemente' not in output and send_notification.upper() == 'S':
         message = client.messages.create(
             to=to,
             from_=from_,
             body=f'Bicicleta disponível tamanho: {bike_size} através do url: {url}'
         )
-    
+
+    with open('results.log', 'a') as file:
+        file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {output}")
+
 except Exception as e:
     raise Exception(f"Error while executing the script: {e}")
